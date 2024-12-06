5de4d9411f6e0a73ad9391209502170c075d1012
ClemeDigs <159574722+ClemeDigs@users.noreply.github.com>
2024-12-06 16:01:42 -0500

    Change contact page

--- app/components/MegaMenu.jsx
+++ app/components/MegaMenu.jsx
@@ -24,16 +24,16 @@ function MenuItem({menuItem}) {
   return (
     <li className="px-3 py-4 group relative text-center">
       {menuItem.items && menuItem.items.length > 0 ? (
-        <div>
+        <>
           <span className="group-focus-within:underline group-hover:underline font-medium cursor-pointer">
             {menuItem.title}
           </span>
-          <ul className="absolute left-[-15px] top-full rounded-b-full bg-pink pt-4 pb-6 transition duration-300 opacity-0 group-hover:opacity-100 flex group-focus-within:opacity-100 shadow-2xl min-w-[120px] flex-col items-center">
+          <ul className="absolute left-[-15px] top-full rounded-b-full bg-pink pt-4 pb-6 transition duration-300 invisible opacity-0 group-hover:visible group-hover:opacity-100 flex group-focus-within:visible group-focus-within:opacity-100 shadow-2xl min-w-[120px] flex-col items-center">
             {menuItem.items.map((subItem, index) => (
               <MenuItem key={index} menuItem={subItem} />
             ))}
           </ul>
-        </div>
+        </>
       ) : (
         <a href={url} className="font-bold">
           {menuItem.title}

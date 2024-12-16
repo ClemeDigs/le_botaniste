254cb21180c093aac6363d3eb18c3121fb629368
ClemeDigs <159574722+ClemeDigs@users.noreply.github.com>
2024-12-16 11:42:57 -0500

    last detail!

--- app/routes/wishList.jsx
+++ app/routes/wishList.jsx
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react';
+import {useState} from 'react';
 import Cookies from 'js-cookie';
 import {useLoaderData, Link} from '@remix-run/react';
 import {Money, Image} from '@shopify/hydrogen';
